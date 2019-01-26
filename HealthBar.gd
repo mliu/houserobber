11813159c7dd5f7d1fe7352dca096a7cc101cfc5
Michael Liu <michaelliu@fb.com>
2019-01-25 21:26:24 -0800

    fix health bar bug temporarily

--- HealthBar.gd
+++ HealthBar.gd
@@ -7,11 +7,11 @@ onready var tween = $HealthTween
 func _ready():
 	pass
 	
-func _process(delta):
-	$HealthBar.value = percentage_fill
+#func _process(delta):
+	# $HealthBar.value = percentage_fill
 
-func _on_update_health(new_value):
-	tween.interpolate_property(self, "percentage_fill", percentage_fill, new_value, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	if not tween.is_active():
-		tween.start()
-	pass
+#func _on_update_health(new_value):
+	# tween.interpolate_property(self, "percentage_fill", percentage_fill, new_value, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	#if not tween.is_active():
+	#	tween.start()
+	#pass
