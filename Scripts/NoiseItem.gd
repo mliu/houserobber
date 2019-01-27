f8887331b700dff20460da60fb9fead53c4ae656
Matt Lim <mlim@fb.com>
2019-01-26 20:57:30 -0800

    Add NoiseItem

---
+++ Scripts/NoiseItem.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _ready():
+	var texture = $TextureRect
+	var shape = RectangleShape2D.new()
+	shape.extents = texture.texture.get_size() / 2
+	$CollisionShape2D.shape = shape
+	$CollisionShape2D.position = texture.texture.get_size() / 2
