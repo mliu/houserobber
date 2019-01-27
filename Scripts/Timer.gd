c2d395ca0f37f757771c48ef2964a2c86f92ff7c
Geoffrey Wang <wgeoffrey@fb.com>
2019-01-26 21:58:04 -0800

    timer function

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -16,3 +16,8 @@ func _process(delta):
 	remaining = time_end - elapsed
 	var str_remaining = "%02d : %02d" % [remaining / 60, remaining % 60]
 	$TimeLeft.text = str_remaining
+	
+	if remaining < (5*60 - 2):
+		$TimeLeft.add_color_override("font_color", "yellow")
+	elif remaining < (5*60 - 5):
+		$TimeLeft.add_color_override("font_color", "red")
